71c26c6998c11083e270ab7b6b27a62f50b4bae0
Francois AUBEUT <francois.aubeut@make.org>
2021-07-16 15:45:23 +0200

    fix(widget): runtime app

--- apps/widget/src/App.tsx
+++ apps/widget/src/App.tsx
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { getQuestionDetails } from '@make.org/store/actions/question/';
 import { getAllProposals } from '@make.org/store/actions/proposals';
+import { loadQuestion } from '@make.org/store/actions/questions';
+import { setCurrentQuestionSlug } from '@make.org/store/actions/currentQuestion';
 import { useAppContext } from '@make.org/store';
 import { KIND_STANDARD } from '@make.org/utils/constants/sequence';
 import { Sequence } from '@make.org/components/Sequence/Sequence';
@@ -12,18 +14,25 @@ const QUESTION_ID = '66a9230b-08cb-4f37-8ed8-aa95a8eac19a';
 
 const App: React.FC = () => {
   const { dispatch, state } = useAppContext();
-  const { currentQuestion } = state;
+  const { currentQuestion, questions } = state;
 
   useEffect(() => {
     const getAllDetails = async () => {
-      const question = await getQuestion(QUESTION_ID);
-      dispatch(getQuestionDetails(question));
-
-      const proposals = await getProposals(question.questionId);
-      dispatch(getAllProposals(proposals));
+      const response = await getQuestion(QUESTION_ID);
+      dispatch(loadQuestion(response));
+      // const proposals = await getProposals(response.questionId);
+      // dispatch(getAllProposals(proposals));
     };
     getAllDetails();
   }, []);
+
+  useEffect(() => {
+    console.log(Object.keys(questions));
+    if (Object.keys(questions).length) {
+      dispatch(setCurrentQuestionSlug(Object.keys(questions)[0]));
+    }
+  }, [questions]);
+  console.log(state);
 
   if (!currentQuestion) {
     return <div>No question yet</div>;
