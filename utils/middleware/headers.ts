7fdf5f440691908289e42809b2e55c812baafcca
Philippe de Araujo <pa@make.org>
2021-09-28 14:28:32 +0200

    fix(global): disable x-frame-options header for widget work

--- utils/middleware/headers.ts
+++ utils/middleware/headers.ts
@@ -9,7 +9,9 @@ export const headersResponseMiddleware = (
     'Strict-Transport-Security',
     'max-age=31536000; includeSubDomains; preload'
   );
-  res.setHeader('X-Frame-Options', 'deny');
+
+  // @toDo
+  // res.setHeader('X-Frame-Options', 'deny');
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-XSS-Protection', '0');
   res.setHeader('Referrer-Policy', 'no-referrer-when-downgrade');
