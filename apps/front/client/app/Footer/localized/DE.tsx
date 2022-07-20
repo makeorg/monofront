23289487b52fa5b0d17224f130393bb0a68f9b3c
Paulin Viel <paulin.viel@make.org>
2022-07-20 10:27:31 +0200

    feat(front): 2847 update wording and footer DE

--- apps/front/client/app/Footer/localized/DE.tsx
+++ apps/front/client/app/Footer/localized/DE.tsx
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import i18n from 'i18next';
+import { trackClickBlog } from '@make.org/utils/services/Tracking';
 import { URL } from '@make.org/types/enums';
 import { FooterLinkType } from './Common';
 
 export const setDEExternalLinks = (isDesktop: boolean): FooterLinkType[] => [
+  {
+    label: i18n.t('main_footer.news'),
+    url: URL.NEWS_LINK_DE,
+    onClick: () => trackClickBlog('blog list'),
+    isDesktop,
+  },
   {
     label: i18n.t('main_footer.jobs'),
     url: URL.JOBS_LINK_DE,
--- types/enums/url.ts
+++ types/enums/url.ts
@@ -7,5 +7,6 @@ export enum URL {
   ABOUT_MAKE_LINK = 'https://about.make.org/',
   DOTATION_FUNDS_LINK = 'https://foundation.make.org',
   NEWS_LINK = 'https://about.make.org',
+  NEWS_LINK_DE = 'https://about.make.org/de/blog',
   PRESS_DETAILS_LINK = 'https://about.make.org/les-medias-en-parlent',
 }
