369a3c8516e2453d394b28a4245bab25ff511691
Philippe de Araujo <pa@make.org>
2023-03-27 16:08:55 +0200

    refactor(front): remove unused featureflip items

--- package/types/enums/featureFlipping.ts
+++ package/types/enums/featureFlipping.ts
@@ -1,15 +1,6 @@
 export enum FEATURE_FLIPPING {
-  OPERATION_MULTI_QUESTIONS_NAVIGATION = 'operation-multi-questions-navigation',
   CONSULTATION_SHARE_DISABLE = 'consultation-share-disable',
-  CONSULTATION_DEPARTMENT_COMPULSORY = 'consultation-department-compulsory',
   CONSULTATION_FOLLOW_US_ACTIVE = 'consultation-follow-us-active',
-  CONSULTATION_POPULAR_TAGS = 'consultation-popular-tags',
-  CONSULTATION_POPULAR_PROPOSALS = 'consultation-popular-proposals',
-  CONSULTATION_SIDEBAR_ACTIVE_ACTORS = 'consultation-sidebar-active-actors',
   CONSULTATION_KEYWORD_ACTIVE = 'keyword-sequence',
   CONSULTATION_TIMELINE_ACTIVE = 'timeline',
-
-  // @todo remove or refactor when Municipales is over
-  MUNICIPAL_TEASING_HEADER = 'consultation-election-teasing-header',
-  MUNICIPAL_PERSONALITY_HEADER = 'consultation-personality-header',
 }
