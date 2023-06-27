87a8c39f407a1ae1306e0b7124bbd33eb18a2fae
Philippe de Araujo <pa@make.org>
2023-06-27 16:00:44 +0200

    feat(content): enable cors and compression

--- apps/content/src/main.ts
+++ apps/content/src/main.ts
@@ -9,17 +9,40 @@ import { Logger as MakeLogger } from '@make.org/content/src/logger';
 import { Logger as NestLogger, ValidationPipe } from '@nestjs/common';
 import { ApiService } from '@make.org/api/ApiService';
 import { ApiServiceServer } from '@make.org/api/ApiService/ApiService.server';
+import compression from 'compression';
+import { NextFunction, Response } from 'express';
 
 const bootstrap = async () => {
   const logger = env.isProduction() ? new MakeLogger() : new NestLogger();
 
   const app = await NestFactory.create(AppModule, { logger });
+  app.enableCors({
+    origin: [
+      /https:\/\/.*\.makeorg\.tech$/,
+      /^https:\/\/.*.make\.org$/,
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+  app.use((req: unknown, res: Response, next: NextFunction) => {
+    res.header('x-powered-by', 'MakeArmada');
+    res.header(
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains; preload',
+    );
+    res.header('X-Content-Type-Options', 'nosniff');
+    res.header('X-XSS-Protection', '0');
+    res.header('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.header('X-Frame-Options', 'deny');
+    next();
+  });
+  app.use(compression());
 
   const config = new DocumentBuilder()
     .setTitle('Make content')
