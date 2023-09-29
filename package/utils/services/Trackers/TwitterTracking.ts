5a95badcc87853ebb31d2f4eb7d354f1f949e601
Olivier Donze <od@make.org>
2023-09-29 11:01:17 +0200

    fix(global): twitter tracking init

--- package/utils/services/Trackers/TwitterTracking.ts
+++ package/utils/services/Trackers/TwitterTracking.ts
@@ -84,7 +84,7 @@ export const TwitterPixel = {
       !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script'); // eslint-disable-line no-use-before-define
 
       // @ts-ignore
-      twq('config', TWITTER_UNIVERSAL_MAKE_TAG);
+      twq('config', twPixelId);
     } catch (e) {
       // @ts-ignore
       Logger.logError(e);
