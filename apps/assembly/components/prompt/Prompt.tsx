14cb922a0ad0936200b0940ee92b9eaf096a13bd
Paulin <paulin.viel@make.org>
2023-12-05 11:52:24 +0100

    feat(assembly): generated prompt style
    closes: MP-3566

--- apps/assembly/components/prompt/Prompt.tsx
+++ apps/assembly/components/prompt/Prompt.tsx
@@ -1,4 +1,135 @@
-import React, { FC } from 'react';
-import { PromptContainer } from './style';
+import React, { FC, useState, useEffect } from 'react';
+import {
+  PromptContainer,
+  GeneratedButtonsContainer,
+  GeneratedButtons,
+  GeneratedButtonText,
+  GeneratedButtonsTitle,
+} from './style';
 
-export const Prompt: FC = () => <PromptContainer>prompt</PromptContainer>;
+const generatedContentArray = [
+  {
+    title: 'test1test1test1 test1test1 test1te',
+    subtitle: 'global',
+    name: 'test1',
+    content: 'content',
+    position: '0',
+  },
+  {
+    title: 'test2',
+    subtitle: 'résumé',
+    name: 'test2',
+    content: 'content',
+    position: '1',
+  },
+  {
+    title: 'test3',
+    subtitle: 'éthique',
+    name: 'test3',
+    content: 'content',
+    position: '2',
+  },
+  {
+    title: 'test4',
+    subtitle: 'aidant',
+    name: 'test4',
+    content: 'content',
+    position: '3',
+  },
+  {
+    title: 'test5',
+    subtitle: 'global',
+    name: 'test5',
+    content: 'content',
+    position: '4',
+  },
+  {
+    title: 'test6',
+    subtitle: 'éthique',
+    name: 'test6',
+    content: 'content',
+    position: '5',
+  },
+  {
+    title: 'test7',
+    subtitle: 'résumé',
+    name: 'test7',
+    content: 'content',
+    position: '6',
+  },
+  {
+    title: 'test8',
+    subtitle: 'global',
+    name: 'test8',
+    content: 'content',
+    position: '7',
+  },
+  {
+    title: 'test9',
+    subtitle: 'résumé',
+    name: 'test9',
+    content: 'content',
+    position: '8',
+  },
+];
+
+const colors = ['#5F5F5F', '#72C083', '#4C41AB', '#7990F1'];
+
+export const Prompt: FC = () => {
+  const [generatedContent, setGeneratedContent] = useState(
+    generatedContentArray
+  );
+  const [contentClicked, setContentClicked] = useState(false);
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const item = colors[randomIndex];
+
+    return item;
+  };
+
+  useEffect(() => {
+    if (generatedContent.length < 5) {
+      setGeneratedContent(generatedContentArray);
+    }
+  }, [generatedContent]);
+
+  const handleClick = (title: string) => {
+    setGeneratedContent(
+      generatedContent.filter(content => content.title !== title)
+    );
+    setContentClicked(true);
+  };
+
+  const renderButtons = contentClicked
+    ? generatedContent.slice(0, 2)
+    : generatedContent.slice(0, 5);
+
+  return (
+    <PromptContainer>
+      <GeneratedButtonsContainer>
+        {renderButtons.map(content => (
+          <GeneratedButtons
+            key={content.title}
+            onClick={() => handleClick(content.title)}
+          >
+            <GeneratedButtonsTitle
+              style={{ backgroundColor: getRandomColor() }}
+            >
+              {content.subtitle}
+            </GeneratedButtonsTitle>
+            <GeneratedButtonText>{content.title}</GeneratedButtonText>
+          </GeneratedButtons>
+        ))}
+        <GeneratedButtons>
+          <GeneratedButtonsTitle style={{ backgroundColor: getRandomColor() }}>
+            thématiques
+          </GeneratedButtonsTitle>
+          <GeneratedButtonText>
+            Découvrir l’ensemble des thématiques
+          </GeneratedButtonText>
+        </GeneratedButtons>
+      </GeneratedButtonsContainer>
+    </PromptContainer>
+  );
+};
--- apps/assembly/components/prompt/style.ts
+++ apps/assembly/components/prompt/style.ts
@@ -12,6 +12,48 @@ export const PromptContainer = styled.div`
   margin: auto auto 0 auto;
   padding: ${spacings.m};
   max-width: ${intToPx(Breakpoints.LargeDesktop)};
-  height: 80px;
-  border: 1px solid blue;
+`;
+
+export const GeneratedButtonsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5px;
+  @media (min-width: ${intToPx(Breakpoints.Tablet)}) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  }
+`;
+
+export const GeneratedButtons = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 5px;
+  padding: 10px;
+  border-style: none;
+  border-radius: 10px;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 4px 18px 0px #0000002b;
+`;
+
+export const GeneratedButtonsTitle = styled.span`
+  text-transform: capitalize;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  font-weight: 600;
+  font-size: 10px;
+  color: white;
+  min-width: 60px;
+  padding: 0 10px;
+  border-radius: 20px;
+  height: 15px;
+`;
+
+export const GeneratedButtonText = styled.p`
+  font-size: 14px;
+  color: #575757;
+  text-align: left;
 `;
