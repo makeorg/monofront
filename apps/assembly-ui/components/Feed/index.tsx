1f3da265e8876f1fdecb88e771656e395768da0b
Paulin <paulin.viel@make.org>
2024-02-01 10:07:31 +0100

    feat(assembly-ui): add stream query
    closes: MP-3691

--- apps/assembly-ui/components/Feed/index.tsx
+++ apps/assembly-ui/components/Feed/index.tsx
@@ -1,16 +1,19 @@
 import React, { FC, useEffect, useLayoutEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import { Question } from './Question';
 import { FeedContainerStyle } from './style';
 import { useAssemblyContext } from '../../store/context';
 import { Answer } from './Answer';
 import { HistoryLimit } from './HistoryLimit';
 import { removeFeedLastItem } from '../../store/feed/actions';
+import { StreamLLM } from '../Prompt/Stream';
 
 export const TRANSCRIPT = 'transcriptStd';
 export const TRANSCRIPT_EXPERT = 'transcriptExpert';
 export const DOCUMENT = 'documentStd';
 export const SOURCE_TYPE_VIDEO = 'VIDEO';
 export const SOURCE_TYPE_DOCUMENT = 'PDF';
+const RESPONSE_TRIGGER_PARAM = 'response-trigger';
 
 const scrollToLastElement = (id: string) => {
   const element = document.getElementById(id);
@@ -27,8 +30,28 @@ const scrollToLastElement = (id: string) => {
 export const Feed: FC = () => {
   const { state, dispatch } = useAssemblyContext();
   const { items } = state.feed;
+  const { termQueries } = state;
   const [maxHistory, setMaxHistory] = useState(false);
   const FEED_MAX_LENGTH = 5;
+
+  const { search } = useLocation();
+  const urlSearchParams = new URLSearchParams(search);
+  const searchQuery = urlSearchParams.get(RESPONSE_TRIGGER_PARAM);
+  const responseTriggerTermQuery = termQueries.find(
+    termQuery => termQuery.title === searchQuery
+  );
+
+  const { setStartStream } = StreamLLM(
+    responseTriggerTermQuery?.value || '',
+    TRANSCRIPT
+  );
+
+  useEffect(() => {
+    if (responseTriggerTermQuery) {
+      setStartStream(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [responseTriggerTermQuery]);
 
   useEffect(() => {
     if (items.length > FEED_MAX_LENGTH) {
